544a9f954be3af770a6a7b49715966dbadf71248
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-09-03 15:01:55 +0200

    Initial commit

---
+++ src/test/java/org/java/spring/alexandria/SpringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.spring.alexandria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
